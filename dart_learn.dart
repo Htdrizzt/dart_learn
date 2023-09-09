76a8302858111001b65b399c09d6c480a26c414e
drizzt_dev <yaser.yalman@gmail.com>
2023-09-09 16:28:44 +0300

    6

--- dart_learn.dart
+++ dart_learn.dart
@@ -1,3 +1,20 @@
 void main() {
-  print('hello world' );
+  int x = 5;
+  String name = 'yaser';
+  var y= 7;
+  print(x*y);
+  var firstName = '$name $y';
+  print(firstName);
+  print(x.toString() + ' ' + name);
+  print('Merhaba $name');
+
+  var nameList = ["yaser", "ahmet", "mehmet", "ali"];
+  print(nameList);
+
+  for (var i = 0; i < nameList.length; i++) {
+    print(nameList[i]);
+  }
+
+  bool isFavourite = true;
+  bool isLoaded = false;
 }
