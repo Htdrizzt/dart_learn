2897693c2549983fecce90cd0e08ca5d62881839
drizzt_dev <yaser.yalman@gmail.com>
2023-09-11 11:54:09 +0300

    7

--- class_learn.dart
+++ class_learn.dart
@@ -1,19 +1,29 @@
 void main() {
-  animals cat = dogs('cat', 16, 'ehe',);
-  animals cat2 = dogs('cat2', 16, 'eyeye',toy: "lego");
+  animals cat = dogs(
+    'cat',
+    16,
+    'ehe',
+  );
+  animals cat2 = dogs('cat2', 16, 'eyeye', toy: "lego");
   cat2.speak();
   cat.speak();
+  print(cat.age);
+ 
+  print(Queue.initialCapacity == 16);
 }
 
-
+class Queue {
+  static const initialCapacity = 16;
+  // ···
+}
 
 class animals {
-  String? name; //instance variables
+  final String name; //instance variables
   int? age; //instance variables
   String _name2 = "alex"; //instance variables
   String? toy;
 
-  animals(this.name, this.age, this._name2 , {this.toy= 'top'});
+  animals(this.name, this.age, this._name2, {this.toy = 'top'});
 
   void speak() // class methods
   {
@@ -22,7 +32,7 @@ class animals {
 }
 
 class dogs extends animals {
-  dogs(super.name, super.age, super.name2,{super.toy});
+  dogs(super.name, super.age, super.name2, {super.toy});
 
   void speak() {
     print('woof woof $name $age $_name2 $toy');
