125a3f63edaba791c7ddaf3f96138c166795f456
drizzt_dev <yaser.yalman@gmail.com>
2023-09-11 03:02:29 +0300

    7

--- class_learn.dart
+++ class_learn.dart
@@ -1,3 +1,33 @@
+void main() {
+
+  var kopek =  animals('köpek');
+
+  kopek.speak();
+}
+
 class animals {
+  String name= '' ;
+  int? age=0 ;
+
+  animals(this.name,{this.age =15});
+
+
+
+  void speak() {
+    print('woof $name $age');
+  }
+}
+
+class dogs extends animals {
+  dogs(super.name,);
+
+
+
+
+  void speak() {
+    print('woof woof');
+  }
 
 }
+
+
